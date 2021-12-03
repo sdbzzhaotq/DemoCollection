ed7525f98bdb4f3b95afd41909fb8e4b3fea8417
zhaotq <ztq519@gmail.com>
2021-12-03 15:30:33 +0800

    modify camera

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # democollecttion
 
 记录工作和学习中的一些简单的demo
+
+## 1.USBCamera
+使用OpenCV采集USB摄像头并展示
--- USBCamera/readAndSave.cpp
+++ USBCamera/readAndSave.cpp
@@ -4,38 +4,42 @@
 #include <ctime>
 
 using namespace std;
-using namespace cv;
-using namespace chrono;
+//using namespace cv;
+//using namespace chrono;
+
+const int ROAD_CAMERA_ID = 0;
+#define FRAME_WIDTH  1164
+#define FRAME_HEIGHT 874
 
 int main(int argc, char** argv) {
+
     //read video
-    cv::VideoCapture capture;
-    capture.open("/dev/video0");
-    capture.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    //VideoCapture capture(ROAD_CAMERA_ID, cv::CAP_V4L2);
+    cv::VideoCapture capture("ns.mov");
+    capture.set(cv::CAP_PROP_FRAME_WIDTH, 853);
     capture.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+    capture.set(cv::CAP_PROP_FPS, 20);
+    capture.set(cv::CAP_PROP_AUTOFOCUS, 0);
+    capture.set(cv::CAP_PROP_FOCUS, 0);
+    float ts[9] = {1.50330396, 0.0, -59.40969163,
+                  0.0, 1.50330396, 76.20704846,
+                  0.0, 0.0, 1.0};
 
-    double dWidth = capture.get(cv::CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
-    double dHeight = capture.get(cv::CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
-    cout << "camera width = " << dWidth << ", height = " << dHeight << endl;
+    assert(capture.isOpened());
 
-    if (!capture.isOpened()) { //check if video device has been initialised
-        cout << "cannot open camera";
-    }
+    cv::Size size(FRAME_WIDTH,FRAME_HEIGHT);
+    const cv::Mat transform = cv::Mat(3, 3, CV_32F, ts);
+    cv::Mat frame_mat, transformed_mat;
 
-    Mat frame;
     while (true) {
-        bool bSuccess = capture.read(frame); // read a new frame from video 
-        //Breaking the while loop if the frames cannot be captured
-        if (bSuccess == false) {
-            cout << "Video camera is disconnected" << endl;
-            cin.get(); //Wait for any key press
-            break;
-        }
-
+        capture >> frame_mat;
+        cv::warpPerspective(frame_mat, transformed_mat, transform, size, cv::INTER_LINEAR, cv::BORDER_CONSTANT, 0);
+        //int transformed_size = transformed_mat.total() * transformed_mat.elemSize();
+        //printf("transformed_size = %d, transformed_mat.data = %p\n",transformed_size,transformed_mat.data);
         //show the frame in the created window
-        imshow("video", frame);
+        imshow("video", transformed_mat);
 
-        if (waitKey(10) == 27) {
+        if (cv::waitKey(10) == 27) {
             cout << "Esc key is pressed by user. Stoppig the video" << endl;
             break;
         }
